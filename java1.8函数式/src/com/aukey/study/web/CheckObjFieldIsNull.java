7a0ad0cd759542029ce610ce8f8d3c5115bc5213
ILoveLinYu <zhangzhijian@aukeys.com>
2018-02-08 12:18:39 +0800

    yy

--- java1.8函数式/src/com/aukey/study/web/CheckObjFieldIsNull.java
+++ java1.8函数式/src/com/aukey/study/web/CheckObjFieldIsNull.java
@@ -18,6 +18,7 @@ public class CheckObjFieldIsNull {
 
 	public static String checkObjFieldIsNull(Object obj) throws IllegalAccessException {
 		String result = "";
+		
 		boolean flag = false;
 		if (obj == null) {
 			flag = true;
